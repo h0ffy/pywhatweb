09448a2cebc985c496597b769dd45c3716fe22cf
h0ffy <45763853+h0ffy@users.noreply.github.com>
2024-03-18 22:12:18 +0100

    manual fix begin

---
+++ whatwebstalker/plugins_manual/4images_plugin.py
@@ -0,0 +1,13 @@
+import sys
+import os
+			
+class 4images_plugin:
+    def get_rules(self):
+        return(self.rules)
+    def __init__(self):
+        self.rules = 
+        [
+			{ "regexp" : "/Copyright &copy; 2002-[0-9]{4} <a href="http:\/\/www.4homepages.de[\>]*>4homepages.de<\/a>/" },
+			{ "version" : "/Powered by <b>4images<\/b> ([\d\.]+)/" },
+		]
+
