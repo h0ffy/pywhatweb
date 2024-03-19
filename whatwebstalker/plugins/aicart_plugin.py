d96bd2bf83ec52d37f76ac51d6cc34b33c640d87
h0ffy <45763853+h0ffy@users.noreply.github.com>
2024-03-19 01:02:07 +0100

    begin manual edit

--- whatwebstalker/plugins/aicart_plugin.py
+++ whatwebstalker/plugins/aicart_plugin.py
@@ -5,9 +5,9 @@ class Pluginaicart_plugin(plugins.Base):
     	pass
     def start(self):
         self.rules = [
-			{ "text" : " for = "l_password">Password <span class="req">*</span></label>" },
-			{ "text" : "<input id = "l_password" name = "l_password" class = "field text medium" type = "password" maxlength = "255" value = " />" },
-			{ "text" : "<input id="l_remember_me" name="l_remember_me" class = "field checkbox" type = "checkbox" value = "1" />" },
-			{ "text" : "<input name="APP_authenticate" type="hidden" id="APP_authenticate" value="frmLogin" />" },
+			{ "text" : "for=\"l_password\">Password <span class=\"req\">*</span></label>" },
+			{ "text" : "<input id = \"l_password\" name = \"l_password\" class=\"field text medium\" type=\"password\" maxlength=\"255\"" },
+			{ "text" : "<input id=\"l_remember_me\" name=\"l_remember_me\" class=\"field checkbox\" type=\"checkbox\" value=\"1\" />" },
+			{ "text" : "<input name=\"APP_authenticate\" type=\"hidden\" id=\"APP_authenticate\" value=\"frmLogin\" />" },
 		]
-	return(self.rules)
+        return(self.rules)
